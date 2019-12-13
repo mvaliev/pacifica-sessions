a634abd3b8f3278e9fc4cbd2226aed69dc1778e7
Marat Valiev <marat.valiev@gmail.com>
2019-12-12 17:34:28 -0800

    Implement scaffolding rest routes.

--- pacifica/session/rest.py
+++ pacifica/session/rest.py
@@ -1,70 +1,53 @@
-"""CherryPy module containing classes for rest interface."""
-from json import dumps
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""This is the module."""
+
+
 import cherrypy
-# from cherrypy import HTTPError
-from pacifica.session.globals import CP_CONFIG_FILE
-
-def error_page_default(**kwargs):
-    """The default error page should always enforce json."""
-    cherrypy.response.headers['Content-Type'] = 'application/json'
-    return dumps({
-        'status': kwargs['status'],
-        'message': kwargs['message'],
-        'traceback': kwargs['traceback'],
-        'version': kwargs['version']
-    })
-
 
 # pylint: disable=too-few-public-methods
-class Dispatch:
-    """CherryPy EventMatch endpoint."""
-
+class SessionDispatch():
+    """ routing for session class. """
     exposed = True
 
-    @staticmethod
+    # pylint: disable=inconsistent-return-statements
+    def __init__(self):
+        self.files = FileDispatch()
+        self.meta = MetaDispatch()
+
+    def _cp_dispatch(self, vpath):
+        if len(vpath) >= 1:
+            session_id = vpath.pop(0)
+            cherrypy.request.params['session_id'] = session_id
+            return self
+
     # pylint: disable=invalid-name
-    def GET():
-        """Get the event ID and return it."""
-        # return str(example_task.delay(method, *numbers))
-        return F'GET received'
-
     @staticmethod
-    # pylint: disable=invalid-name
-    def POST(name):
-        """Get the event ID and return it."""
-        # return str(example_task.delay(method, *numbers))
-        return name
+    def GET(session_id=None):
+        """GET method"""
+        return F'Session session_id={session_id}'
 
-
-# pylint: disable=too-few-public-methods
-class Status:
-    """CherryPy Receive Event object."""
-
+class FileDispatch():
+    """/files route"""
     exposed = True
 
-    @staticmethod
     # pylint: disable=invalid-name
-    def GET(uuid):
-        """Receive the event and dispatch it to backend."""
-        return F'GET {uuid}'
-        # try:
-        #     return str(ExampleModel.get(uuid=uuid).value)
-        # except DoesNotExist:
-        #     raise HTTPError('404', 'Not Found')
-# pylint: enable=too-few-public-methods
+    @staticmethod
+    def GET(session_id=None):
+        """GET method"""
+        return F'Files session_id={session_id}'
 
-
-# pylint: disable=too-few-public-methods
-class Root:
-    """CherryPy Root Object."""
-
+class MetaDispatch():
+    """ /meta route """
     exposed = True
-    dispatch = Dispatch()
-    status = Status()
-# pylint: enable=too-few-public-methods
+
+    # pylint: disable=invalid-name
+    @staticmethod
+    def GET(session_id=None):
+        """GET method"""
+        return F'Metadata session_id={session_id}'
 
 if __name__ == '__main__':
-    from pacifica.session import _PROJ_DIR
-    print(_PROJ_DIR)
-    CONFIG_FILE = CP_CONFIG_FILE
-    cherrypy.quickstart(Root(), '/', CONFIG_FILE)
+    from pacifica.session.globals import CP_CONFIG_FILE
+
+    cherrypy.quickstart(SessionDispatch(), '/', CP_CONFIG_FILE)
--- tests/rest_test.py
+++ tests/rest_test.py
@@ -2,11 +2,13 @@
 # -*- coding: utf-8 -*-
 """This is the test module."""
 
-
+import uuid
 import cherrypy
 import pytest
 import requests
-from pacifica.session.rest import Root
+
+
+from pacifica.session.rest import SessionDispatch
 from pacifica.session.globals import CP_CONFIG_FILE
 
 @pytest.fixture(scope="module")
@@ -14,7 +16,7 @@ def run_server():
     """server setup."""
     config_file = CP_CONFIG_FILE
     cherrypy.config.update(config_file)
-    cherrypy.tree.mount(Root(), '/', config_file)
+    cherrypy.tree.mount(SessionDispatch(), '/sessions', config_file)
 
     # set localhost and port if needed
     cherrypy.server.socket_host = '127.0.0.1'
@@ -32,11 +34,41 @@ def run_server():
         cherrypy.engine.exit()
         cherrypy.engine.block()
 
-
-# pylint: disable=redefined-outer-name
-def test_post(run_server):
+#pylint: disable=redefined-outer-name
+def test_sessions_get(run_server):
     """test post"""
-    endpoint = '/dispatch/marat'
+    session_id = None
+    endpoint = F'/sessions'
     url = run_server
-    resp = requests.post(F'{url}{endpoint}')
-    assert resp.text == 'marat'
+    resp = requests.get(F'{url}{endpoint}')
+    ref_text = F'Session session_id={session_id}'
+    assert resp.text == ref_text
+
+def test_sessions_id_get(run_server):
+    """test post"""
+    session_id = uuid.uuid4()
+
+    endpoint = F'/sessions/{session_id}'
+    url = run_server
+    resp = requests.get(F'{url}{endpoint}')
+    ref_text = F'Session session_id={session_id}'
+    assert resp.text == ref_text
+
+
+def test_file_get(run_server):
+    """test post"""
+    session_id = uuid.uuid4()
+    endpoint = F'/sessions/{session_id}/files'
+    url = run_server
+    resp = requests.get(F'{url}{endpoint}')
+    ref_text = F'Files session_id={session_id}'
+    assert resp.text == ref_text
+
+def test_meta_get(run_server):
+    """test post"""
+    session_id = uuid.uuid4()
+    endpoint = F'/sessions/{session_id}/meta'
+    url = run_server
+    resp = requests.get(F'{url}{endpoint}')
+    ref_text = F'Metadata session_id={session_id}'
+    assert resp.text == ref_text
